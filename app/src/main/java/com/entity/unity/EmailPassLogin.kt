0a0dfeacd076866d9909a4ab1a2bc84ee8037e2a
Akshai Baruah <akshaib10@gamil.com>
2021-12-30 00:28:51 +0530

    Login screen : beta

---
+++ app/src/main/java/com/entity/unity/EmailPassLogin.kt
@@ -0,0 +1,11 @@
+package com.entity.unity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailPassLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_pass_login)
+    }
+}
