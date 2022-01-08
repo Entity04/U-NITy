09aeddb968d8c206de787889ff7654ed8876c870
narotam26 <nsv261102@gmail.com>
2022-01-08 10:42:51 +0530

    chatcode2

--- app/src/main/java/com/entity/unity/ChattingActivity.kt
+++ app/src/main/java/com/entity/unity/ChattingActivity.kt
@@ -1,11 +1,94 @@
 package com.entity.unity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ChattingActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    var recieverRoom: String?=null
+    var senderRoom: String?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
+
+        val name= intent.getStringExtra("name")
+        val recieverUid= intent.getStringExtra("uid")
+
+
+        val senderUid= FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef= FirebaseDatabase.getInstance().getReference()
+
+        senderRoom= recieverUid+senderUid
+        recieverRoom=senderUid+recieverUid
+
+        supportActionBar?.title= name
+
+
+
+        chatRecyclerView=findViewById(R.id.chatRecyclerView)
+        messageBox=findViewById(R.id.messageBox)
+        sendButton=findViewById(R.id.sentButton)
+        messageList= ArrayList()
+        messageAdapter= MessageAdapter(this,messageList)
+
+        chatRecyclerView.layoutManager= LinearLayoutManager(this)
+        chatRecyclerView.adapter=messageAdapter
+
+        sendButton.setOnClickListener{
+            val message=messageBox.text.toString()
+            val messageObject=Message()
+        }
+
+
+        mDbRef.child("chats").child(senderRoom!!).child("messages")
+            .addValueEventListener(object : ValueEventListener {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    messageList.clear()
+
+                    for (postSnapshot in snapshot.children){
+                        val message=postSnapshot.getValue(Message::class.java)
+                        messageList.add(message!!)
+
+                    }
+                    messageAdapter.notifyDataSetChanged()
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
+
+
+
+        sendButton.setOnClickListener {
+            val message = messageBox.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(recieverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+
+            messageBox.setText("")
+        }
     }
 }
